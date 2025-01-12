f83c9bfdb3e4fad2e37c6d4c8c4a795b032105aa
Brian Walborn <brian.walborn@gmail.com>
2025-01-12 16:43:37 -0500

    Update YAML references, add argo root app

--- kubernetes/scripts/06-argo-cd.sh
+++ kubernetes/scripts/06-argo-cd.sh
@@ -2,4 +2,5 @@
 
 kubectl create namespace argocd
 kubectl -n argocd apply -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl -n argocd apply -f argocd-ingress.yaml
+kubectl -n argocd apply -f ../manifests/argocd/ingress.yaml
+kubectl -n argocd apply -f ../manifests/argocd/root-app.yaml
--- kubernetes/scripts/07-cloudflared.sh
+++ kubernetes/scripts/07-cloudflared.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 kubectl create namespace cloudflared
-kubectl -n cloudflared apply -f cloudflared.yaml
+kubectl -n cloudflared apply -f ../manifests/cloudflared/cloudflared.yaml
